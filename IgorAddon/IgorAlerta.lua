1a351f161b279ad436b31cd235be8da140c30e28
Igor Nunes Rosseto (Ir0ns1d3) <54159112+igornrosseto@users.noreply.github.com>
2019-08-17 03:56:59 -0300

    Update IgorAlerta.lua

--- IgorAddon/IgorAlerta.lua
+++ IgorAddon/IgorAlerta.lua
@@ -1,38 +1,42 @@
-SLASH_TURNOFF1 = "/turnoff1"
-SlashCmdList["TURNOFF"] = function()
- 		DisableAddOn("IgorAddon")
- 	end
-
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 f:SetScript("OnEvent", function(self, event)
 	-- pass a variable number of arguments
-	self:OnEvent(event, CombatLogGetCurrentEventInfo())
+self:OnEvent(event, CombatLogGetCurrentEventInfo())
 end)
 
-function f:OnEvent(event, ...)
+ function f:OnEvent(event, ...)
 	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
-	englishFaction = UnitFactionGroup(sourceName)
-	level = UnitLevel(sourceName)
-	englishClass = UnitClass(sourceName)
+
+	local playerGUID = sourceGUID
 	
-	if englishFaction == "Horde" then
-		
+	local localizedClass, englishClass, localizedRace, englishRace, sex, name, realm = GetPlayerInfoByGUID(playerGUID)
+
+	
   StaticPopupDialogs["ALERTA_PVP"] = {
-  text = "Alerta!: Inimigo na area",
-  --  sourceName," - ", level, englishClass -- 
+  text = "Alerta! - %s - %s",
   button1 = "Dispensar",
-  -- OnAccept = , --
   timeout = 5,
   whileDead = true,
   hideOnEscape = true,
 }
 
+	if englishRace == "NightElf" then
+	StaticPopup_Show ("ALERTA_PVP", englishRace, englishClass)	
+	end
 
-StaticPopup_Show ("ALERTA_PVP")	
+	if englishRace == "Human" then
+	StaticPopup_Show ("ALERTA_PVP", englishRace, englishClass)
+	end
 
+	if englishRace == "Dwarf" then
+	StaticPopup_Show ("ALERTA_PVP", englishRace, englishClass)	
+	end
+
+	if englishRace == "Gnome" then
+	StaticPopup_Show ("ALERTA_PVP", englishRace, englishClass)	
+	end
 
 end
 
-end
